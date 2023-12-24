701201732a9007e4ebf91d4b7ea2ee74322c7f7f
al-ramos <80824351+al-ramos@users.noreply.github.com>
2023-12-23 22:10:14 -0300

    Alex

--- LanchesMac/obj/Debug/netcoreapp3.1/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/netcoreapp3.1/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c839ea2b1c240fa7b4a5ecd96ca7ff27e8471722")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
